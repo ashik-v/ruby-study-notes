f48f713dd1a94ec2a84d3bb6bff6dedf8269ac27
Ashik Varghese <ashikv@nulogy.com>
2021-04-12 20:26:14 -0400

    solved head first ruby exercise on page 11

---
+++ head-first-ruby-p-11.rb
@@ -0,0 +1,31 @@
+# Head First Ruby
+# Page 11
+
+puts "Enter name: "
+name = gets.chomp
+
+puts "Welcome, #{name}!"
+
+target = rand(100) + 1
+
+number_of_guesses = 0
+
+while number_of_guesses < 10
+  puts "Enter guess: "
+  guess = gets.chomp.to_i
+  case
+  when guess < target
+    puts "Oops. Your guess was LOW."
+  when guess > target
+    puts "Oops. Your guess was HIGH."
+  when guess == target
+    puts "Good job, #{name}! You guessed my number in #{number_of_guesses} guesses!"
+    exit
+  end
+  number_of_guesses += 1
+end
+
+if number_of_guesses > 10
+  puts "Sorry. You didn't get my number. My number was #{target}."
+  exit
+end
