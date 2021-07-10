4e79e5af01cf5a917bd46e5316be9303a6ec01a6
Ashik Varghese <ashikv@nulogy.com>
2021-07-10 09:19:06 -0700

    HFR Ch 13 and 14: complete web app

--- movies/lib/movie_store.rb
+++ movies/lib/movie_store.rb
@@ -1,23 +1,31 @@
 require 'yaml/store'
 
 class MovieStore
+  attr_reader :store
+
   def initialize(filename)
     @store = YAML::Store.new(filename)
   end
 
   def save(movie)
-    @store.transaction do
+    store.transaction do
       unless movie.id
-        highest_id = @store.roots.max || 0
+        highest_id = store.roots.max || 0
         movie.id = highest_id + 1
       end
-      @store[movie.id] = movie
+      store[movie.id] = movie
     end
   end
 
   def all
-    @store.transaction do
-      @store.roots.map{ |id| @store[id] }
+    store.transaction do
+      store.roots.map{ |id| @store[id] }
+    end
+  end
+
+  def find(id)
+    store.transaction do
+      store[id]
     end
   end
 end
